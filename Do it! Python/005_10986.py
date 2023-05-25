46f9d190177ca4405ac9ee979bb9a53b22aeb498
Jeongju Park <parkjeongju140@gmail.com>
2023-05-26 00:34:53 +0900

    second commit

--- Do it! Python/005_10986.py
+++ Do it! Python/005_10986.py
@@ -2,3 +2,24 @@ import sys
 input=sys.stdin.readline
 
 n,m=map(int,input().split())
+
+val=list(map(int,input().split()))
+Sval=[0]*n
+Sval[0]=val[0]
+Clsit=[0]*m
+cnt=0
+
+for i in range(1,n):
+    Sval[i]=Sval[i-1]+val[i]
+
+for i in range(n):
+    x=Sval[i]%m
+    if x==0:
+        cnt+=1
+    Clsit[x]+=1
+
+for i in range(m):
+    if Clsit[i]>1:
+        cnt+=(Clsit[i]*(Clsit[i]-1)//2)
+
+print(cnt)
